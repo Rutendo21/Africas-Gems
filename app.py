ece53dc16e397680022150187e27fef24fb577fa
Rutendo21 <rutendomureriwa@gmail.com>
2023-04-15 11:46:05 +0200

    Newer Commit

--- app.py
+++ app.py
@@ -182,7 +182,7 @@ def submit():
 
             d = request.form.get("cast member").title()
 
-            db.execute("INSERT INTO series (image, title, year, screenwriter, acountry, acity, scountry, scity, description, cast1, cast2) VALUES (?,?,?,?,?,?,?,?,?,?,?)", path, title, year, screenwriter, author_country, author_city, setting_country, setting_city, description, c, d)
+            db.execute("INSERT INTO series (image, title, year, screenwriter, acountry, acity, scountry, scity, description, cast1, cast2) VALUES (?,?,?,?,?,?,?,?,?,?,?)", image, title, year, screenwriter, author_country, author_city, setting_country, setting_city, description, c, d)
 
             db.execute("INSERT INTO titles (title, category) VALUES (?,?)", title, category)
             
